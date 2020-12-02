a930e21671f9722b6fff96ddbc7775c56894845b
leegopc <dzzzzzb@gmail.com>
2020-12-02 17:46:22 +0800

    equal computation

--- script.js
+++ script.js
@@ -18,21 +18,53 @@ class Calculator{
   }
   
   appendNumber(number){
-
+    if(this.currentOperand.includes('.') && number === '.') return
     this.currentOperand = this.currentOperand.toString()+number.toString()
 
   }
 
   update(){
-     
       this.current.innerText = this.currentOperand
+      this.previous.innerText = this.previousOperand
   }
 
-  chooseOperation(){
+  chooseOperation(operation){
+      if(this.currentOperand === '') return 
+      if(this.previousOperand !== ''){
+          this.compute()
+      }
+      this.operation = operation
+      this.previousOperand = this.currentOperand
+      this.currentOperand = ''
 
   }
 
   compute(){
+
+    let computation
+    const prev = parseFloat(this.previousOperand)
+    const curr = parseFloat(this.currentOperand)
+    if (isNaN(prev) || isNaN(curr)) return
+    switch (this.operation){
+        case '+':
+            computation = prev + curr
+            break
+        case '-':
+            computation = prev - curr
+            break
+        case '*':
+            computation = prev * curr
+            break
+        case '/':
+            computation = prev / curr
+            break
+        default: 
+         return    
+    }
+
+    this.currentOperand = computation;
+    this.previousOperand ='';
+    this.operation = undefined;
 
 
 
@@ -59,4 +91,18 @@ numberButtons.forEach(button => {
       
     })
   })
+
+
+  operationButtons.forEach(button => {
+      button.addEventListener('click', ()=>{
+          calculator.chooseOperation(button.innerText)
+          calculator.update()
+      })
+  })
   
+
+  equalsButton .addEventListener('click', button => {
+      calculator.compute();
+      calculator.update();
+
+      })
